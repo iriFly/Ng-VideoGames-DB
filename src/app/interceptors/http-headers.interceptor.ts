3869954d41ada0841cb61c188e716db409621cdd
Irina Afanassenko <irina.afanassenko@gmail.com>
2022-10-20 14:26:15 +0200

    interceptors and api added

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,22 @@
+
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': '64827f9a33c349bfb572053f3acbb315',
+        'x-rapidapi-host': 'https://api.rawg.io/api/games',
+      },
+      setParams: {
+        key: '64827f9a33c349bfb572053f3acbb315',
+      }
+    });
+    return next.handle(req);
+  }
+}
